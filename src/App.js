5d50fd239da5b3d55a1056566060c0521289d419
Weston Farmer <wesfarmer11@gmail.com>
2024-03-22 19:11:13 -0500

    GetQuoteproperly sets the state of quote and can render it to UI, but there is still an invalid hook call

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-
+  
+  
   return (
     <div className="App">
-      <button onClick={GetQuote}>Click</button>
+     <button onClick={GetQuote}>New Quote</button>
     </div>
   );
 }
 
 function GetQuote() {
   const [quote, setQuote] = useState(null);
-
-  useEffect(() => {
-
-    fetch("https://api.quotable.io/random")
-    .then(resp => resp.json())
-    .then((data) => {
-      setQuote(data[0].quote)
+  
+  fetch("https://api.quotable.io/random")
+  .then(resp => resp.json())
+  .then((data) => {
+      setQuote(data.content);
       console.log(data);
     })
-    .catch((error) => console.log(error));
-  }, []);
-
-    return (
+  .catch((error) => console.log(error));
+  
+  return (
       <div>
         <h2>Quote of the Day: </h2>
         <p>{quote}</p>
       </div>
-    )
+  )
 }
+
 export default App;
