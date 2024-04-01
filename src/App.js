0b0ae65ad3a19a605a074008dfc9f37d1b559faf
Weston Farmer <wesfarmer11@gmail.com>
2024-04-01 09:10:53 -0500

    Button now renders to DOM successfully with corrected syntax. nextQuoteClickHandler is called when button is clicked

--- src/App.js
+++ src/App.js
@@ -3,34 +3,37 @@ import './App.css';
 import Button from './Components/Button';
 
 function App() {
-  
-  
   return (
     <div className="App" id="quote-box">
-      <h2 id="text"></h2>
-      <h4 id="author"></h4>
-     <Button buttonDisplayName="new-quote" id="new-quote"/>
-    </div>
-  );
+        <Button buttonDisplayName="New Quote" clickHandler={nextQuoteClickHandler}/>
+      </div>
+      //Button is currently not rendering to the DOM, I am not sure why yet.
+      //Fixed! The parentheses vs curly bracket syntax was not matching what the tutorial did. With parens, the return is implicit
+      //Next step is creating clickHandler to be passed to Button component
+      );
+    }
+    
+function nextQuoteClickHandler() {
+  console.log("Clicked!");
 }
 
-function GetQuote() {
-  const [quote, setQuote] = useState(null);
+// function GetQuote() {
+//   const [quote, setQuote] = useState(null);
   
-  fetch("https://api.quotable.io/random")
-  .then(resp => resp.json())
-  .then((data) => {
-      setQuote(data.content);
-      console.log(data);
-    })
-  .catch((error) => console.log(error));
+//   fetch("https://api.quotable.io/random")
+//   .then(resp => resp.json())
+//   .then((data) => {
+//       setQuote(data.content);
+//       console.log(data);
+//     })
+//   .catch((error) => console.log(error));
   
-  return (
-      <div>
-        <h2>Quote of the Day: </h2>
-        <p>{quote}</p>
-      </div>
-  )
-}
+//   return (
+//       <div>
+//         <h2>Quote of the Day: </h2>
+//         <p>{quote}</p>
+//       </div>
+//   )
+// }
 
 export default App;
