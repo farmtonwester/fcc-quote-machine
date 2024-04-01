9e4e5c3e00f3521c718b122cdb3e137d0dd54a47
Weston Farmer <wesfarmer11@gmail.com>
2024-04-01 11:44:25 -0500

    installed packages for tweet button styles, and added href to IconButton for tweet functionality

--- src/Components/QuoteMachine.js
+++ src/Components/QuoteMachine.js
@@ -4,21 +4,27 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-const QuoteMachine = (props) => (
+
+
+const QuoteMachine = ({ assignNewQuoteIndex, selectedQuote }) => (
     <Card>
         <CardContent>
-            { 
-                props.selectedQuote ? 
-                (
-                    <Typography>
-                        {props.selectedQuote.quote} - {props.selectedQuote.author}
-                    </Typography>
-                ) : null
-            }
+            <Typography>
+                {selectedQuote.quote} - {selectedQuote.author}
+            </Typography>
         </CardContent>
        <CardActions>
-            <Button size="small" onClick={props.assignNewQuoteIndex} id="new-quote">New Quote</Button>
+            <Button size="small" onClick={assignNewQuoteIndex} id="new-quote">New Quote</Button>
+            <IconButton
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?text=${selectedQuote.quote}`}
+            >
+                <FontAwesomeIcon icon={faTwitter} size="md"></FontAwesomeIcon>
+            </IconButton>
        </CardActions>
     </Card>
     
